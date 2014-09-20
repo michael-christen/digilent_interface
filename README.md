605c82aaa3e37f83400a69188fd688515b0b3a6c
karatekid <mikechri@umich.edu>
2014-09-19 22:26:08 -0700

    got analog input working, note ch 2 = idx 1

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Interfacing with the Digilent Analog Discovery
 * The shared library is located @ '/usr/lib/libdwf.so.2.7.5' 
 * The header file is located    @ '/usr/local/include/digilent/waveforms/dwf.h'
+* Command line executable is 'dwfcmd'
+* Analog input ie) dwfcmd connect debug=2  pause=2 ai single freq=100000 channel=2 enable=1 range=2 samples=5000 start finish save=ao.txt show
+
--- main.c
+++ main.c
@@ -3,17 +3,65 @@
 	Purpose: Interface with Digilent SDK
 	Date: 7/20/2014
 */
-#include "main.h"
+#include <stdlib.h>
 #include<digilent/waveforms/dwf.h>
+
+#include "main.h"
+
+#define TRUE  1
+#define FALSE 0
 
 
 int main(int argc, char *argv[]) {
 	int numDevices = 0;
 	FDwfEnum(enumfilterAll, &numDevices);
+	/*
 	printf("Num devices: %d\n", numDevices);
 	printf("Num args: %d\n", argc);
 	for(int i = 0; i < argc; ++i) {
 		printf("Arg %d: %s\n", i, argv[i]);
 	}
+	*/
+	HDWF device;
+	FDwfDeviceOpen(-1, &device);
+	//Start stuff
+	FDwfAnalogInReset(device);
+	FDwfAnalogInAcquisitionModeSet(device, acqmodeSingle);
+	double frequency = 100000;
+	const int channel = 1;
+	int bufSize = 5000;
+
+	FDwfAnalogInFrequencySet(device, frequency);
+	FDwfAnalogInChannelEnableSet(device, channel, TRUE);
+	FDwfAnalogInChannelRangeSet(device, channel, 2);
+
+	FDwfAnalogInBufferSizeSet(device, bufSize);
+	//Get actual buffer size
+	FDwfAnalogInBufferSizeGet(device, &bufSize);
+	FDwfAnalogInFrequencyGet(device, &frequency);
+	FDwfAnalogInRecordLengthSet(device, bufSize / frequency);
+
+	sleep(1);
+
+	//Start
+	FDwfAnalogInConfigure(device, TRUE, TRUE);
+	DwfState devState = DwfStateRunning;
+	//Finish
+	while(devState != DwfStateDone) {
+		FDwfAnalogInStatus(device, TRUE, &devState);
+	}
+	//get data
+	double * data = malloc(sizeof(double) * bufSize);
+	FDwfAnalogInStatusData(device,channel, data, bufSize); 
+	for(int i = 0; i < bufSize; ++i) {
+		printf("%d\t%f\n", i, data[i]);
+	}
+
+	//Stop instrument
+	FDwfAnalogInConfigure(device, FALSE, FALSE);
+
+	//free data & close
+	free(data);
+	FDwfDeviceCloseAll();
 	return 0;
 }
